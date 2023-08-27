cd2897f692349001b0a6a4a58c302638b48650e8
Prith Manickam <prith77@outlook.com>
2023-08-27 14:43:24 +0100

    Added Friends Comparison View in Dashboard Page
    - added a friends table in the bottom of the dashboard
    - once comparison icon is clicked, you can view yours vs friends radar and bar charts

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -16,6 +16,8 @@ import {
 import InfoIcon from "@mui/icons-material/Info";
 import DeleteIcon from "@mui/icons-material/Delete";
 import PublishIcon from "@mui/icons-material/Publish";
+import CompareIcon from "@mui/icons-material/Compare";
+
 import {
   Radar,
   RadarChart,
@@ -56,16 +58,20 @@ const barChartData = [
 const initalRoutineHistoryData = [
   {
     title: "Efficient Full Body Workout",
-    workouts: [{ title: "Push-ups", duration: "3 sets x 15 reps", area:"arms" },
-               { title: "squats", duration: "4 sets x 15 reps", area:"legs" },
-               { title: "Jumping Jacks", duration: "4 sets x 20 reps", area: "Cardio" }],
+    workouts: [
+      { title: "Push-ups", duration: "3 sets x 15 reps", area: "arms" },
+      { title: "squats", duration: "4 sets x 15 reps", area: "legs" },
+      { title: "Jumping Jacks", duration: "4 sets x 20 reps", area: "Cardio" },
+    ],
     time: 45,
     date: "2023-08-25",
   },
   {
     title: "My Legs Workout",
-    workouts: [{ title: "Push-ups", duration: "3 sets x 15 reps", area: "legs" },
-               { title: "calves", duration: "3 sets x till failure", area: "legs" }],
+    workouts: [
+      { title: "Push-ups", duration: "3 sets x 15 reps", area: "legs" },
+      { title: "calves", duration: "3 sets x till failure", area: "legs" },
+    ],
     time: 30,
     date: "2023-08-25",
   },
@@ -73,10 +79,7 @@ const initalRoutineHistoryData = [
     title: "Intense Cardio Workout",
     summary:
       "Elevate your heart rate and burn calories with this cardio routine.",
-    workouts: [
-      { title: "Running", duration: "20 minutes", area: "Cardio" },
-      
-    ],
+    workouts: [{ title: "Running", duration: "20 minutes", area: "Cardio" }],
     time: 20,
     date: "2023-08-24",
   },
@@ -112,6 +115,52 @@ const initalCreatedRoutineData = [
   // Add more created routine data
 ];
 
+const friendsData = [
+  {
+    name: "Alice",
+    radarData: [
+      { category: "Core", value: 3 },
+      { category: "Chest", value: 2 },
+      { category: "Back", value: 4 },
+      { category: "Legs", value: 2 },
+      { category: "Cardio", value: 5 },
+      { category: "Arms", value: 3 },
+      { category: "Shoulders", value: 2 },
+    ],
+    barChartData: [
+      { day: "Mon", time: 60 },
+      { day: "Tue", time: 45 },
+      { day: "Wed", time: 50 },
+      { day: "Thu", time: 70 },
+      { day: "Fri", time: 80 },
+      { day: "Sat", time: 90 },
+      { day: "Sun", time: 50 },
+    ],
+  },
+  {
+    name: "Bob",
+    radarData: [
+      { category: "Core", value: 4 },
+      { category: "Chest", value: 3 },
+      { category: "Back", value: 2 },
+      { category: "Legs", value: 4 },
+      { category: "Cardio", value: 3 },
+      { category: "Arms", value: 2 },
+      { category: "Shoulders", value: 3 },
+    ],
+    barChartData: [
+      { day: "Mon", time: 50 },
+      { day: "Tue", time: 70 },
+      { day: "Wed", time: 40 },
+      { day: "Thu", time: 80 },
+      { day: "Fri", time: 70 },
+      { day: "Sat", time: 100 },
+      { day: "Sun", time: 60 },
+    ],
+  },
+  // Add more friend data
+];
+
 const DashboardPage = () => {
   const [openInfoDialog, setOpenInfoDialog] = React.useState(false);
   const [deleteRoutineDialogOpen, setDeleteRoutineDialogOpen] =
@@ -131,6 +180,9 @@ const DashboardPage = () => {
   const [savedRoutineData, setSavedRoutineData] = React.useState(
     initalSavedRoutineData
   );
+
+  const [openCompareDialog, setOpenCompareDialog] = React.useState(false);
+  const [selectedFriend, setSelectedFriend] = React.useState(null);
 
   const handleInfoClick = (routine) => {
     setSelectedRoutine(routine);
@@ -157,6 +209,16 @@ const DashboardPage = () => {
     setRoutineToDelete(null);
   };
 
+  const handleCompareClick = (friend) => {
+    setSelectedFriend(friend);
+    setOpenCompareDialog(true);
+  };
+
+  const handleCloseCompareDialog = () => {
+    setOpenCompareDialog(false);
+    setSelectedFriend(null);
+  };
+
   return (
     <Container maxWidth="l">
       <Container
@@ -168,6 +230,9 @@ const DashboardPage = () => {
           elevation={3}
           style={{ flex: 1, padding: "20px", marginRight: "20px" }}
         >
+          <Typography variant="h6" style={{ marginBottom: "10px" }}>
+            Workout Areas
+          </Typography>
           <RadarChart
             outerRadius={90}
             width={300}
@@ -211,6 +276,9 @@ const DashboardPage = () => {
           elevation={3}
           style={{ flex: 1, padding: "20px", marginLeft: "20px" }}
         >
+          <Typography variant="h6" style={{ marginBottom: "10px" }}>
+            Workout time during last 7 Days
+          </Typography>
           <ResponsiveContainer width={300} height={300}>
             <BarChart data={barChartData}>
               <XAxis dataKey="day" />
@@ -343,6 +411,161 @@ const DashboardPage = () => {
           </List>
         </Paper>
       </Container>
+
+      {/* Friends Section */}
+      <Paper elevation={3} style={{ padding: "20px", marginBottom: "20px" }}>
+        <Typography variant="h6" style={{ marginBottom: "10px" }}>
+          Friends
+        </Typography>
+        <List>
+          {friendsData.map((friend, index) => (
+            <ListItem
+              key={index}
+              style={{
+                borderRadius: "12px",
+                marginBottom: "10px",
+                background: "#f3f3f3",
+                border: "2px solid #e49797",
+                padding: "10px",
+              }}
+            >
+              <ListItemText primary={friend.name} />
+              <IconButton onClick={() => handleCompareClick(friend)}>
+                <CompareIcon />
+              </IconButton>
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+
+      {/* Dialog for Comparison */}
+      <Dialog
+        open={openCompareDialog}
+        onClose={handleCloseCompareDialog}
+        maxWidth="md"
+      >
+        {selectedFriend && (
+          <div>
+            <DialogTitle
+              style={{
+                fontSize: "24px", // Set the desired font size
+                textDecoration: "underline", // Add underline
+              }}
+            >
+              Comparison with {selectedFriend.name}
+            </DialogTitle>
+            <DialogContent>
+              {/* Radar Chart */}
+              <div style={{ display: "flex" }}>
+                <div style={{ marginRight: "20px" }}>
+                  <Typography variant="h6" style={{ marginBottom: "10px" }}>
+                    Your Workout Areas
+                  </Typography>
+                  <RadarChart
+                    outerRadius={90}
+                    width={300}
+                    height={300}
+                    data={radarData} // Use your own data here
+                  >
+                    <PolarGrid />
+                    <PolarAngleAxis dataKey="category" />
+                    <PolarRadiusAxis />
+                    <Radar
+                      dataKey="value"
+                      fill="#8884d8" // Your data color
+                      fillOpacity={0.6}
+                    />
+                  </RadarChart>
+                </div>
+                <div>
+                  <Typography variant="h6" style={{ marginBottom: "10px" }}>
+                    {selectedFriend.name}'s Workout Areas
+                  </Typography>
+
+                  <RadarChart
+                    outerRadius={90}
+                    width={300}
+                    height={300}
+                    data={selectedFriend.radarData} // Friend's data
+                  >
+                    <PolarGrid />
+                    <PolarAngleAxis dataKey="category" />
+                    <PolarRadiusAxis />
+                    <Radar
+                      dataKey="value"
+                      fill="#ff0000" // Friend's data color
+                      fillOpacity={0.6}
+                    />
+                  </RadarChart>
+                </div>
+              </div>
+
+              {/* Bar Charts */}
+              <Typography variant="h6" style={{ margin: "20px 0" }}>
+                Workout time during last 7 Days
+              </Typography>
+              <div style={{ display: "flex" }}>
+                <ResponsiveContainer width={300} height={300}>
+                  <BarChart data={barChartData} layout="vertical">
+                    {" "}
+                    {/* Use your own data here */}
+                    <XAxis dataKey="time" />
+                    <YAxis dataKey="day" type="category" />
+                    <Tooltip />
+                    <Bar dataKey="time" fill="#8884d8" />{" "}
+                    {/* Your data color */}
+                  </BarChart>
+                </ResponsiveContainer>
+                {selectedFriend.barChartData && (
+                  <ResponsiveContainer width={300} height={300}>
+                    <BarChart
+                      data={selectedFriend.barChartData} // Friend's data
+                      layout="vertical"
+                    >
+                      <XAxis dataKey="time" />
+                      <YAxis dataKey="day" type="category" />
+                      <Tooltip />
+                      <Bar dataKey="time" fill="#ff0000" />{" "}
+                      {/* Friend's data color */}
+                    </BarChart>
+                  </ResponsiveContainer>
+                )}
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: "10px",
+                  margin: "10px",
+                }}
+              >
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <div
+                    style={{
+                      width: "15px",
+                      height: "15px",
+                      backgroundColor: "#8884d8", // Your data color
+                      marginRight: "5px",
+                    }}
+                  />
+                  <span>Your Data</span>
+                </div>
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <div
+                    style={{
+                      width: "15px",
+                      height: "15px",
+                      backgroundColor: "#ff0000", // Friend's data color
+                      marginRight: "5px",
+                    }}
+                  />
+                  <span>{selectedFriend.name}'s Data</span>
+                </div>
+              </div>
+            </DialogContent>
+          </div>
+        )}
+      </Dialog>
 
       {/* Dialog for Routine Details */}
       <Dialog open={openInfoDialog} onClose={() => setOpenInfoDialog(false)}>
